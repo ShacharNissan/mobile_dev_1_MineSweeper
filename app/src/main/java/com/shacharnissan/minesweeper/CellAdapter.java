93df318fc3c9991213a409de4d8a1ff5429954a1
Shachar Nissan <Shachar208@gmail.com>
2021-04-13 18:31:41 +0300

    CellAdapter and CellView finished

--- app/src/main/java/com/shacharnissan/minesweeper/CellAdapter.java
+++ app/src/main/java/com/shacharnissan/minesweeper/CellAdapter.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.GridView;
 
 import com.shacharnissan.minesweeper.logic.Board;
 import com.shacharnissan.minesweeper.logic.Game;
@@ -12,9 +13,14 @@ public class CellAdapter extends BaseAdapter {
     private Board board;
     private Context context;
 
+    private int cachedWidth;
+    private int cachedHeight;
+
     public CellAdapter(Board board, Context context){
         this.board = board;
         this.context = context;
+        this.cachedHeight = -1;
+        this.cachedWidth = -1;
     }
 
     @Override
@@ -34,6 +40,42 @@ public class CellAdapter extends BaseAdapter {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        return null;
+        CellView cell = null;
+        if(convertView != null)
+            cell = (CellView)convertView;
+        else
+            cell = new CellView(context);
+
+        GridView gridView = (GridView)parent;
+
+        if(cachedHeight <= 0) {
+            calculateSizes(gridView);
+        }
+
+        ViewGroup.LayoutParams layoutParams = new GridView.LayoutParams(cachedWidth,cachedHeight);
+
+        cell.setLayoutParams(layoutParams);
+
+
+        ////////////////
+        //cell.textView.setText(board.getTile(position).getStateString());
+        return cell;
+    }
+    private void calculateSizes(GridView gridView) {
+
+        int hSpacing =  gridView.getHorizontalSpacing();
+        int vSpacing =  gridView.getVerticalSpacing();
+
+        int columns = gridView.getNumColumns();
+        int rows = board.getBoardSize() / columns;
+
+        int hPaddingSize = hSpacing * (columns - 1);
+        int vPaddingSize = vSpacing * (rows - 1);
+
+        cachedWidth = (gridView.getWidth() - hPaddingSize) / columns;
+
+
+        cachedHeight = (gridView.getHeight() - vPaddingSize) / rows;
+
     }
 }
--- app/src/main/java/com/shacharnissan/minesweeper/CellView.java
+++ app/src/main/java/com/shacharnissan/minesweeper/CellView.java
@@ -1,10 +1,29 @@
 package com.shacharnissan.minesweeper;
 
 import android.content.Context;
+import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 
 public class CellView extends LinearLayout {
+    ImageView imageview;
     public CellView(Context context) {
         super(context);
+
+        this.setOrientation(VERTICAL);
+
+        imageview = new ImageView(context);
+
+        LayoutParams layoutParams = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+        imageview.setLayoutParams(layoutParams);
+
+        //imageview.setTextAlignment(TEXT_ALIGNMENT_CENTER);
+        //imageview.setForegroundGravity(Gravity.CENTER_VERTICAL);
+        //imageview.setTextSize(50);
+        //imageview.setTextColor(Color.BLACK);
+        //setBackground(getResources().getDrawable(R.drawable.mine,null));
+
+        addView(imageview);
+
     }
 }
