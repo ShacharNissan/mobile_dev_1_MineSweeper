ad28fb52f039d3a90382c09681228765c91ad32b
edenGlu <45977312+edenGlu@users.noreply.github.com>
2021-04-14 13:29:00 +0300

    Fix

--- app/src/main/java/com/shacharnissan/minesweeper/StartActivity.java
+++ app/src/main/java/com/shacharnissan/minesweeper/StartActivity.java
@@ -74,11 +74,13 @@ public class StartActivity extends AppCompatActivity {
     private void getHighestResults() {
         String filename = getResources().getString(R.string.score_filename);
         SharedPreferences sharedPref = getSharedPreferences(filename,Context.MODE_PRIVATE);
+
+        // for reset all highest results:
+        // sharedPref.edit().clear().commit();
+
         long easyHighestResult = sharedPref.getLong(getString(R.string.easy_score_tag), 0);
         long medHighestResult = sharedPref.getLong(getString(R.string.med_score_tag), 0);
         long hardHighestResult = sharedPref.getLong(getString(R.string.hard_score_tag), 0);
-//      
-//        sharedPref.edit().clear().commit();
 
         // put values timers:
         easy_score_text.setText(writeTimeClockFormat(easyHighestResult));
